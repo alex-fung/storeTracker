50d70fa8643c29199da8662d0d71e091583576ee
alex-fung <alexcfung@gmail.com>
2014-09-21 04:08:15 -0400

    frontend interface

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -6,30 +6,61 @@ $(document).ready(function(){
 	var options = {
 	};
 
+	var data1 = [];
+	var data2 = [];
+	var data3 = [];
+	var intLabels = [];
+
+	$.getJSON("http://storetracker.herokuapp.com/api/location_points/1", function(data){
+		data1 = data;
+	});
+
+	$.getJSON("http://storetracker.herokuapp.com/api/location_points/2", function(data){
+		data2 = data;
+	});
+
+	$.getJSON("http://storetracker.herokuapp.com/api/location_points/3", function(data){
+		data3 = data;
+	});
+
+	for(var i = 0; i < data1.length; i++){
+		intLabels[i] = i + 1;
+	}
+
 	var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: intLabels,
     datasets: [
         {
-            label: "My First dataset",
+            label: "First Beacon",
             fillColor: "rgba(220,220,220,0.2)",
             strokeColor: "rgba(220,220,220,1)",
             pointColor: "rgba(220,220,220,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: data1
         },
         {
-            label: "My Second dataset",
+            label: "Second Beacon",
             fillColor: "rgba(151,187,205,0.2)",
             strokeColor: "rgba(151,187,205,1)",
             pointColor: "rgba(151,187,205,1)",
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-		        }
-		    ]
+            data: data2
+        },
+        {
+            label: "Thord Beacon",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: data3
+        }    
+      ]
 	};
 	var myLineChart = new Chart(ctx).Line(data, options);
 });
